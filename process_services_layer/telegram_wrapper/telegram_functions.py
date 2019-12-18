48b49f04b59a4ed0bb6b2e6ee6932c1eec64ea7d
Davide <davide.zanella10@gmail.com>
2019-12-18 18:40:12 +0100

    Add user registration [telegram_wrapper]

--- process_services_layer/telegram_wrapper/telegram_functions.py
+++ process_services_layer/telegram_wrapper/telegram_functions.py
@@ -1,10 +1,23 @@
-import os
+import os, requests
 from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
 
 
+register_user = 'http://register_user/register'
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    user = {
+        'user_id': update.message.from_user.id,
+        'username': update.message.from_user.username,
+        'first_name': update.message.from_user.first_name,
+        'last_name': update.message.from_user.last_name,
+        'chat_id': update.effective_chat.id
+    }
+    
+    context.bot.send_message(chat_id=user['chat_id'], text="Hi {}! I'll help you tracking flights!".format(user['first_name']))
+
+    #register user and chat
+    r = requests.post(register_user, json=user)
+    assert r.status_code == 200
 
 
 def unknown(update, context):
